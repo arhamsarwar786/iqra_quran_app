8871d6e23dd002f9b832b3fcb8fd9d97b7263bb4
Arham Sarwar <arhamsarwar786@gmail.com>
2026-03-01 11:30:15 +0500

    names of allah

--- lib/Models/name_of_Allah_model.dart
+++ lib/Models/name_of_Allah_model.dart
@@ -4,42 +4,46 @@
 
 import 'dart:convert';
 
-List<NameOfAllahModel> nameOfAllahModelFromJson(String str) => List<NameOfAllahModel>.from(json.decode(str).map((x) => NameOfAllahModel.fromJson(x)));
+List<NameOfAllahModel> nameOfAllahModelFromJson(String str) =>
+    List<NameOfAllahModel>.from(
+        json.decode(str).map((x) => NameOfAllahModel.fromJson(x)));
 
-String nameOfAllahModelToJson(List<NameOfAllahModel> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+String nameOfAllahModelToJson(List<NameOfAllahModel> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class NameOfAllahModel {
-    int sr;
-    String? transliteration;
-    String? namesOfAllahInUrdu;
-    String? namesOfAllahInArabic;
-    String? urduMeaning;
-    String? englishMeaning;
+  int sr;
+  String? transliteration;
+  String? namesOfAllahInUrdu;
+  String? namesOfAllahInArabic;
+  String? urduMeaning;
+  String? englishMeaning;
 
-    NameOfAllahModel({
-        required this.sr,
-        required this.transliteration,
-        required this.namesOfAllahInUrdu,
-        required this.namesOfAllahInArabic,
-        required this.urduMeaning,
-        required this.englishMeaning,
-    });
+  NameOfAllahModel({
+    required this.sr,
+    required this.transliteration,
+    required this.namesOfAllahInUrdu,
+    required this.namesOfAllahInArabic,
+    required this.urduMeaning,
+    required this.englishMeaning,
+  });
 
-    factory NameOfAllahModel.fromJson(Map<String, dynamic> json) => NameOfAllahModel(
+  factory NameOfAllahModel.fromJson(Map<String, dynamic> json) =>
+      NameOfAllahModel(
         sr: json["SR#"],
         transliteration: json["Transliteration"],
         namesOfAllahInUrdu: json["Names of Allah in Urdu"],
-        namesOfAllahInArabic: json["Names of Allah in Arabic"],
+        namesOfAllahInArabic: json["Names of Allah in Arabic"],
         urduMeaning: json["Urdu Meaning"],
         englishMeaning: json["English Meaning"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "SR#": sr,
         "Transliteration": transliteration,
         "Names of Allah in Urdu": namesOfAllahInUrdu,
-        "Names of Allah in Arabic": namesOfAllahInArabic,
+        "Names of Allah in Arabic": namesOfAllahInArabic,
         "Urdu Meaning": urduMeaning,
         "English Meaning": englishMeaning,
-    };
+      };
 }
